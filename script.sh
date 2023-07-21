4ddd3edaf2beab605027518f438703e53c828b31
aartdem <artdem243@gmail.com>
2023-07-21 21:30:25 +0300

    Add optimization levels

--- script.sh
+++ script.sh
@@ -25,8 +25,10 @@ fi
 
 assembly_directory="assemblies" # hardcoded so far
 mkdir -p $assembly_directory
+declare -a opt_lvls=("O0" "O1" "O2" "O3")
 
 for cur_instr_dir in "$tests_directory"/* ; do
+    
     instr_name=$(basename -- "$cur_instr_dir")
 
     if ! [ -d "$cur_instr_dir" ] ; then
@@ -37,19 +39,23 @@ for cur_instr_dir in "$tests_directory"/* ; do
     echo "Instruction $instr_name:"
 
     for test_file in "$cur_instr_dir"/*.c; do
-        cur_asm=${test_file%.*}.s # change extension
-        cur_asm=$assembly_directory/${cur_asm#*/} # change first directory 
         
+        asm_dir=$assembly_directory/${test_file#*/} # change first directory 
         mkdir -p $assembly_directory/"$instr_name"
+    
+        for i in {0..3} ; do
 
-        $compiler -march="rv64gc_zba_zbb_zbc_zbs" -S -o "$cur_asm" "$test_file"
+            cur_asm=${asm_dir%.*}-${opt_lvls[i]}.s # change extension and optimization level to filename
+            
+            $compiler -march="rv64gc_zba_zbb_zbc_zbs" -S -"${opt_lvls[i]}" -o "$cur_asm" "$test_file"
 
         # if [ $? -eq 0 ]; then
         #     echo "Test ${test_file%.*} compiled successfully"
             
         #     objdump -d ${test_file%.*} | grep -oP '(?<=<)[^>]+'
         # fi
+        done
     done
 done
 
-rm -rf $assembly_directory
+# rm -rf $assembly_directory
