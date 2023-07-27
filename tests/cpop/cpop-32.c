1da10be60c110ab802967cc053203a6c0804fbac
unknown <lanovaya.al6@gmail.com>
2023-07-27 15:44:29 +0300

    Fix bugs with word lenght in tests

--- tests/cpop/cpop-32.c
+++ tests/cpop/cpop-32.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
 
+#define XLEN 32
+
 int32_t test(int32_t rs) {
     int32_t count = 0;
-    for (int i = 0; i < 64; ++i)
-        if (rs & (1ll << i)) 
+    for (int i = 0; i < XLEN; ++i)
+        if (rs & (1 << i)) 
             ++count;
     return count;
 }
--- tests/rol/rol-32.c
+++ tests/rol/rol-32.c
@@ -1,8 +1,10 @@
 #include <stdint.h>
 
+#define XLEN 32
+
 uint32_t test(uint32_t rs1, uint32_t rs2) {
     uint32_t shamt = rs2;
-    for (int i = 12; i < 64; ++i) 
+    for (int i = 12; i < XLEN; ++i) 
     	shamt &= ~((uint32_t)1 << i);
-    return (rs1 << shamt) | (rs1 >> (64 - shamt));
+    return (rs1 << shamt) | (rs1 >> (XLEN - shamt));
 }
