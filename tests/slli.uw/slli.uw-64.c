e2d369ae61879d06b95d4c4143f01cb11da68eea
aartdem <artdem243@gmail.com>
2023-07-28 06:13:39 +0300

    Add last tests

--- tests/slli.uw/slli.uw-64.c
+++ tests/slli.uw/slli.uw-64.c
@@ -1,16 +1,6 @@
 #include <stdint.h>
 
-/*
-Below implemetation is correct:
-        sllw    a0, a0, a1
-        zext.w  a0, a0
-        ret
-see bitmanip 2.41.slli.uw Architecture Explanation
-it's equivalent to slli.uw
-*/
-
-uint64_t test(int64_t rs1, int shamt) {
-    uint32_t word = rs1;
-    uint64_t res = word << shamt;
+uint32_t test(uint32_t rs1, uint32_t shamt) {
+    uint32_t res = rs1 << shamt;
     return res;
 }
